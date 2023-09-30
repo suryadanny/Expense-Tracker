8c931978698ee8a086c09ab8891f394c5d4229e6
unknown <dhaneshwarlingam@gmail.com>
2023-09-30 15:10:01 -0500

    add api to add contacts and get All contacts

--- src/main/java/org/expense/demo/api/UserApp.java
+++ src/main/java/org/expense/demo/api/UserApp.java
@@ -1,17 +1,26 @@
 package org.expense.demo.api;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.ws.soap.AddressingFeature.Responses;
 
+import org.expense.demo.model.Contact;
 import org.expense.demo.model.User;
 import org.expense.demo.repo.UserRepository;
 import org.expense.demo.service.OTPService;
+import org.expense.demo.utility.Utility;
 
 @Path("user")
 public class UserApp {
@@ -48,6 +57,34 @@ public class UserApp {
      		return Response.Status.fromStatusCode(500);
 		}
 		return Response.Status.CREATED;
+	}
+	
+	@Path("/addFriend")
+	@POST
+	public Response addUserToNetwork(@Context HttpHeaders headers , @QueryParam("username") String username){
+		try {
+			Map<String, String> map = Utility.resolveHeaders(headers.getHeaderString("Authorization"));
+			userRepo.addFriend(Integer.parseInt(map.get("userId")), username);
+
+		} catch (Exception ex) {
+			return Response.serverError().entity(ex.getMessage()).build();
+		}
+		return Response.ok("CREATED").build();
+
+	}
+	
+	@Path("/getAllFriends")
+	@GET
+	public Response getAllFriends(@Context HttpHeaders headers) {
+		List<Contact> contactList = new ArrayList<Contact>();
+		try {
+			Map<String,String> map = Utility.resolveHeaders(headers.getHeaderString("Authorization"));
+			contactList = userRepo.getAllFriends(Integer.parseInt(map.get("userId")));
+		}catch(Exception ex) {
+			return Response.serverError().entity(ex).build();
+		}
+		return Response.ok(contactList).build();
+		
 	}
 	
 }
---
+++ src/main/java/org/expense/demo/model/Contact.java
@@ -0,0 +1,45 @@
+package org.expense.demo.model;
+
+public class Contact {
+	private int id;
+	private String username;
+	private String name;
+	private String email;
+	private String mobile;
+	public int getId() {
+		return id;
+	}
+	public Contact setId(int id) {
+		this.id = id;
+		return this;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public Contact setUsername(String username) {
+		this.username = username;
+		return this;
+	}
+	public String getName() {
+		return name;
+	}
+	public Contact setName(String name) {
+		this.name = name;
+		return this;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public Contact setEmail(String email) {
+		this.email = email;
+		return this;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public Contact setMobile(String mobile) {
+		this.mobile = mobile;
+		return this;
+	}
+	
+}
