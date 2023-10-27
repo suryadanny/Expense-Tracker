462e2de632015cc6936609fc2ced182de10d72b3
Rakshak Gowda <rakshakgowda@gmail.com>
2023-10-27 15:26:56 -0500

    added additional group func

--- src/main/webapp/js/Groups.js
+++ src/main/webapp/js/Groups.js
@@ -9,21 +9,22 @@ function getAllGroups() {
 //   }
     groupsList = [
          {
-             "groupID": "1",
-             "userIdList": ['1', '4'],
+             "groupID": 1,
+             "userIdList": [1, 4],
              "groupName": "Movie",
-             "amt_owed": "20",
+             "amt_owed": 20,
          },
 
          {
-             "groupID": "1",
-              "userIdList": ['1', '6'],
+             "groupID": 2,
+              "userIdList": [1, 6],
               "groupName": "Taxi",
-              "amt_owed": "-70",
+              "amt_owed": -70,
          },
     ]
 
-    let horizontal_bar_open = "<div class=\"horizontal-bar\">";
+    let horizontal_bar_open_part1 = "<div class=\"horizontal-bar\" onclick=\"updateGroupExpenseFunc(";
+    let horizontal_bar_open_part2 = ")\">";
     let firstColOpenAndClose = "<div class=\"col-5\"></div>";
     let secondColOpen = "<div class=\"col-4\">";
     let closeDiv = "</div>";
@@ -40,12 +41,12 @@ function getAllGroups() {
         let grpID = groupsList[i]["groupID"];
         let amt = groupsList[i]["amt_owed"];
         let color = "green";
-        if (amt[0] === "-"){
+        if (amt < 0){
         color = "red";
-        amt = amt.substring(1);
+        amt = amt * -1;
         }
         let newRow =
-            horizontal_bar_open +
+            horizontal_bar_open_part1 + grpID + horizontal_bar_open_part2 +
             firstColOpenAndClose +
             secondColOpen +
             nameEntryDiv + name + closeDiv +
@@ -57,32 +58,150 @@ function getAllGroups() {
         allGroupsDetails += newRow;
     }
 
-    document.getElementById("friendsContent").innerHTML = allGroupsDetails;
+    document.getElementById("groupsContent").innerHTML = allGroupsDetails;
 }
 
-
-function generateFriendListCheckbox() {
-    var myParent = document.getElementById("newGrpFriendsList");
+function populateListOfPotentialPayers(grpUserIdsList) {
+    let myParent = document.getElementById("listOfPotentialPayersPlaceholder");
     let url = "/app/user/getAllFriends";
     let friendsList = loadJson(url)
-//    var selectList = document.createElement("newGrpFriendsList");
-//    selectList.id = "newGrpFriendsList";
-//    selectList.name = "newGrpFriendsList";
-//    myParent.appendChild(selectList);
+    let selectList = document.createElement("select");
+    selectList.id = "selectedPayer";
+    selectList.name = "selectedPayer";
+    myParent.appendChild(selectList);
+
+    let option = document.createElement("option");
+    option.value = getCookie("userID");
+    option.text = "Me";
+    selectList.appendChild(option);
 
     //Create and append the options
-    for (var i = 0; i < friendsList.length; i++) {
-        var checkbox = document.createElement("input");
-        var checkboxID = "checkbox" + friendsList[i]["id"]
+    for (let i = 0; i < friendsList.length; i++) {
+        if (grpUserIdsList.includes(friendsList[i]["id"])){
+            let option = document.createElement("option");
+            option.value = friendsList[i]["id"];
+            option.text = friendsList[i]["name"];
+            selectList.appendChild(option);
+        }
+    }
+}
+
+function populateListOfPotentialPayees(grpUserIdsList) {
+    let myParent = document.getElementById("listOfPotentialPayersPlaceholder");
+    let url = "/app/user/getAllFriends";
+    let friendsList = loadJson(url)
+    let selectList = document.createElement("select");
+    selectList.id = "selectedPayer";
+    selectList.name = "selectedPayer";
+    myParent.appendChild(selectList);
+
+    let option = document.createElement("option");
+    option.value = getCookie("userID");
+    option.text = "Me";
+    selectList.appendChild(option);
+
+    //Create and append the options
+    for (let i = 0; i < friendsList.length; i++) {
+        if (grpUserIdsList.includes(friendsList[i]["id"])){
+            let option = document.createElement("option");
+            option.value = friendsList[i]["id"];
+            option.text = friendsList[i]["name"];
+            selectList.appendChild(option);
+        }
+    }
+}
+
+function updateGroupExpenseFunc(groupID) {
+//    let url = "/app/user/getAllFriends";
+//    let friendsList = loadJson(url) // send query to get user expenditure
+//   {
+//     groupID
+//     List<UserId> userIdList
+//      group name
+//   	amt_owed
+//   }
+    groupsList = [
+             {
+                 "groupID": 1,
+                 "userIdList": [1, 4],
+                 "groupName": "Movie",
+                 "amt_owed": 20,
+             },
+
+             {
+                 "groupID": 2,
+                  "userIdList": [1, 6],
+                  "groupName": "Taxi",
+                  "amt_owed": -70,
+             },
+        ]
+    let grpUserIdsList = [];
+    for (let i=0; i<groupsList.length; i++) {
+        if(groupID === groupsList[i]["groupID"]){
+            grpUserIdsList = groupsList[i]["userIdList"];
+            break;
+        }
+    }
+    populateListOfPotentialPayers(grpUserIdsList);
+    generateFriendListCheckbox("listOfPotentialPayeesPlaceHolder", grpUserIdsList);
+    openTab('updateGroupExpenseForm');
+    console.log(groupID);
+
+}
+
+function submitNewGroup()
+{
+    let newGrpName = document.getElementById("newGrpName").value;
+    let usersIDs = [getCookie("userID")];
+    let checkedBoxes = document.querySelectorAll('input[name=newUserCheckbox]:checked');
+    for (let i=0; i<checkedBoxes.length; i++){
+        usersIDs.push(checkedBoxes[i].value);
+    }
+    let a = 0;
+    let payload = {
+        grpName: newGrpName,
+        userIdList: usersIDs
+    }
+    let payloadJson = JSON.stringify(payLoad);
+//    let url = "/app/user/addFriend?username=" + userName;
+//
+//    sendInfo(url, payloadJson, false);
+}
+
+function generateFriendListCheckbox(parentEleID, listOfFriendIDsInGroup) {
+    let myParent = document.getElementById(parentEleID);
+    let url = "/app/user/getAllFriends";
+    let allFriendsList = loadJson(url);
+
+    let checkboxName = "newUserCheckbox";
+    let friendsList = allFriendsList;
+    if(listOfFriendIDsInGroup !== null){
+        checkboxName = "addGrpExpenseCheckBox";
+        friendsList = [];
+        for(let i=0; i < allFriendsList.length; i++){
+            if(listOfFriendIDsInGroup.includes(allFriendsList[i]["id"])){
+                friendsList.push(allFriendsList[i]);
+            }
+        }
+    }
+
+    //Create and append the options
+    for (let i = 0; i < friendsList.length; i++) {
+        let checkbox = document.createElement("input");
+        let checkboxID = checkboxName + friendsList[i]["id"]
         checkbox.type = "checkbox";
-        checkbox.name = checkboxID;
+        checkbox.name = checkboxName;
         checkbox.value = friendsList[i]["id"];
         checkbox.id = checkboxID;
-        var label = document.createElement("label");
-        label.for = checkboxID
+        let label = document.createElement("label");
+        label.setAttribute("for",checkboxID);
         label.innerHTML = friendsList[i]["name"]
         myParent.appendChild(checkbox);
         myParent.appendChild(label);
     }
 }
-generateFriendListCheckbox()
+
+function bodyOnLoad() {
+    getAllGroups();
+    generateFriendListCheckbox("newGrpFriendsList", null);
+}
